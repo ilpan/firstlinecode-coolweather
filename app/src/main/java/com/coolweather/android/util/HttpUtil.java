33ea835f786263f07c528443b9b39a21d3083cf0
Pan Naian <pna110@126.com>
2017-02-04 22:16:45 +0800

    完成遍历加载省市县三级列表的功能

---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,21 @@
+package com.coolweather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by ilpan on 2017/2/4.
+ *
+ * func: 与服务器进行交互
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(address)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+}
