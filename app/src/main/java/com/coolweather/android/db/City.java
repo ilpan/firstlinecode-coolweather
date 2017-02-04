e8059b7586d4da132a1ae3fad8ec1b50fd24c3b9
Pan Naian <pna110@126.com>
2017-02-04 18:57:13 +0800

    litepal配置

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -7,4 +7,45 @@ import org.litepal.crud.DataSupport;
  */
 
 public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private String cityCode;    // 记录市的代号
+
+    private int provinceId;     // 记录当前市所属省的 id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
 }
--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -7,4 +7,34 @@ import org.litepal.crud.DataSupport;
  */
 
 public class Province extends DataSupport {
+
+    private int id;                 // 每个实体类都该有的代号
+
+    private String provinceName;    // 记录省的名字
+
+    private String provinceCode;    // 记录省的代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
 }
